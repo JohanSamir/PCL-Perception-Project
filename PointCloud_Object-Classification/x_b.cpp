a5c3eb57ee40803342893072d687d0220eab1819
johan <jobando0730@gmail.com>
2019-04-10 23:59:37 -0500

    Sum

--- PointCloud_Object-Classification/x_b.cpp
+++ PointCloud_Object-Classification/x_b.cpp
@@ -808,6 +808,30 @@ std::vector <std::vector <float> > mat_mult(std::vector <std::vector <float> > m
     return mult;
 }
 
+std::vector <std::vector <float> > mat_sum(std::vector <std::vector <float> > m1, std::vector <std::vector <float> > m2){
+    int r1 = fil_size(m1);
+    int r2 = fil_size(m2);
+    int c1 = col_size(m1);
+    int c2 = col_size(m2);
+    std::vector <std::vector <float> > sum;
+
+    sum = crear_matriz(r1, c2);
+
+    if(r1 == r2 && c1 == c2){
+        //cout << "La suma resultante sera de: " << r1 << "x" << c2 << endl;
+        for(int i = 0; i < r1; ++i){
+            for(int j = 0; j < c2; ++j)
+            {
+                sum[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+    }
+    else{
+        cout << "No corresponden las filas o las columnas" << endl;
+    }
+    return sum;
+}
+
 
 
 
